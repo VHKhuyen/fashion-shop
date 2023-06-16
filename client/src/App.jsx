7be46a336a1f5f2d03b9ddfaca042665c4d59d9b
vukhuyen <92353606+VHKhuyen@users.noreply.github.com>
2023-06-16 09:37:06 +0700

    change router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,71 @@
-import { RouterProvider } from "react-router-dom";
-import { routes } from "./routes/index";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { requestShop } from "./utils/httpRequest";
 import "./assets/styles/textstyles.css";
+import Main from "./layouts/Main";
+import {
+  Login,
+  Register,
+  Home,
+  Products,
+  ProductPage,
+  StoreLocation,
+  Contact,
+  CartPage,
+  Wishlist,
+  ErrorPage,
+} from "./pages";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/products/:id",
+        loader: async ({ params }) =>
+          await requestShop.get(
+            `http://localhost:8000/api/v1/products/${params.id}`
+          ),
+
+        element: <ProductPage />,
+      },
+
+      {
+        path: "/stores",
+        element: <StoreLocation />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/wish-list",
+        element: <Wishlist />,
+      },
+      {
+        path: "/cart",
+        element: <CartPage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
