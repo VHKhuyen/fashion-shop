30bf8c5ab631ac27864880acab03b55739c3ac78
vukhuyen <92353606+VHKhuyen@users.noreply.github.com>
2023-06-17 15:30:17 +0700

    for deploy

--- server/src/db/init.mysql.js
+++ server/src/db/init.mysql.js
@@ -1,8 +1,7 @@
 const { Sequelize } = require("sequelize");
-const {
-  db: { host, name, password, user, dialect, pool },
-} = require("../config/config.mysql");
+const config = require("../config/config.mysql");
 
+const { host, name, password, user, dialect, pool } = config.db;
 const sequelize = new Sequelize(name, user, password, {
   host: host,
   dialect: dialect,
