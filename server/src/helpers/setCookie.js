65e4f3d1e0779489351760f61d86e4e12ff06c73
vukhuyen <92353606+VHKhuyen@users.noreply.github.com>
2023-06-26 15:13:03 +0700

    test set-cookie

--- server/src/helpers/setCookie.js
+++ server/src/helpers/setCookie.js
@@ -6,6 +6,7 @@ const setCookie = (res, accessToken, refreshToken) => {
     httpOnly: true,
     secure: true,
     sameSite: "strict",
+    domain: "jenta.site",
     maxAge: accessTokenMaxAge,
   });
 
@@ -13,6 +14,7 @@ const setCookie = (res, accessToken, refreshToken) => {
     httpOnly: true,
     secure: true,
     sameSite: "strict",
+    domain: "jenta.site",
     maxAge: refreshTokenMaxAge,
   });
 };
